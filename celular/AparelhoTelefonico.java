b12f19d54673e251e29dee70d81e518dece1679b
Aline <likasemog@gmail.com>
2023-10-11 11:06:07 -0300

    projeto iphone

---
+++ celular/AparelhoTelefonico.java
@@ -0,0 +1,7 @@
+package celular;
+
+public interface AparelhoTelefonico {
+    public void ligar(String numero);
+    public void atender(String numero);
+    public void iniciarCorreioVoz(String numero);
+}
